3c1fe509aac1ea634488fc73c51ff52e9fd36661
Vincent Thomason <p.vincent.thomason@gmail.com>
2021-11-29 10:06:16 -0500

    product card sizing fix

--- src/components/productcard/ProductCard.js
+++ src/components/productcard/ProductCard.js
@@ -5,8 +5,8 @@ import { ReactComponent as RightArrow } from "../../assets/r-arr.svg";
 const ProductCard = (props) => {
     return (
         <div className="productcard">
-            <img src={props.image} alt="coffee" className={`productcard__img productcard__img${props.id}`} />
             <div className="productcard__infocontainer">
+                <img src={props.image} alt="coffee" className={`productcard__img productcard__img${props.id}`} />
                 <div className="productcard__textbox">
                     <h3 className="heading-3">{props.name}</h3>
                     <p className="paragraph">Serving the best coffee in town. Every one likes coffee.</p>
